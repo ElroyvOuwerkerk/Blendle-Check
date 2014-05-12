b33179b1a4a984e0a761e0698e5741c2808d2b34
Mtvanb <menno@Mennos-MacBook-Pro.local>
2014-05-12 22:10:33 +0200

    Added a Blendle-article check

--- background.js
+++ background.js
@@ -4,9 +4,9 @@
 
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
-chrome.tabs.executeScript(null, { file: "jquery.js" }, function() {
-    chrome.tabs.executeScript(null, { file: "content.js" });
-});
+	chrome.tabs.executeScript(null, { file: "jquery.js" }, function() {
+	    chrome.tabs.executeScript(null, { file: "content.js" });
+	});
 });
 
 
--- content.js
+++ content.js
@@ -1,3 +1,8 @@
+var url = $(location).attr('href');
 var title = $( ".l-overlay .item-title" ).html();
-var provider = $( ".l-overlay .item-provider" ).html(); 
-alert("title: " + title + "\nprovider: " + provider);
+var provider = $( ".l-overlay .item-provider" ).html();
+if (url.indexOf("beta.blendle.nl/#item") == -1){
+	alert("Weet je zeker dat je een artikel op Blendle leest?");
+} else{
+	alert("title: " + title + "\nprovider: " + provider);
+}
