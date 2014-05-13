58eadd865e796476cb1ba2902086a08fe19488fe
Mtvanb <menno@Mennos-MacBook-Pro.local>
2014-05-13 08:59:54 +0200

    Removed comments

--- background.js
+++ background.js
@@ -1,12 +1,5 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
 	chrome.tabs.executeScript(null, { file: "jquery.js" }, function() {
 	    chrome.tabs.executeScript(null, { file: "content.js" });
 	});
 });
-
-
